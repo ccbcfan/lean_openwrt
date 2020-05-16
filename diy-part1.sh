9599f959f5f8ab3645a6007008ecec252f7a9ed3
ccbcfan <5327204@qq.com>
2020-05-16 21:17:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,4 +12,4 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git lienol https://github.com/TinyTitanPro/lienol-openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/kenzok8/openwrt-packages' feeds.conf.default
