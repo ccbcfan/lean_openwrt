a87094c71633d885a424940dfb00ae6548c83790
ccbcfan <5327204@qq.com>
2020-05-16 21:39:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,5 +11,7 @@
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
-sed -i '$a src-git lienol https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# Add a feed source (passwall.ipk)
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
