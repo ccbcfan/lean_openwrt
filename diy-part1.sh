de0a90848f41da34d05809289a6084a4833a64b9
ccbcfan <5327204@qq.com>
2020-06-15 22:31:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,10 +12,11 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source (passwall.ipk)
-sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+#sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 #sed -i '$a src-git luci-app-dockerman https://github.com/lisaac/luci-app-dockerman' feeds.conf.default
 git clone https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
 git clone https://github.com/kenzok8/small package/kenzol8/small
 git clone https://github.com/kenzok8/openwrt-packages package/kenzol8/package
 git clone https://github.com/Lienol/openwrt-package package/lienol
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
